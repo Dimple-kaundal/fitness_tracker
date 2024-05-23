f66a64890c9e3203ca9c1d67b4fbca5503e3f743
Balraj Singh <balrajsingz1002@gmail.com>
2024-05-23 21:38:16 +0530

    Update Diets.jsx

--- src/Components/JSX_Files/Workouts&Dietplans/Diets.jsx
+++ src/Components/JSX_Files/Workouts&Dietplans/Diets.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import '../../CSS_Files/Diets.css'
 
-const Diets = () => {
-
+const Diets = ({ diets }) => {
   return (
     <>
+    <div className="diets">
+        <h1></h1>
+        <div className="dietsContainer">
+          {diets.map((item) => (
+            <div className="dietsplans" key={item.id}>
+              <h2>{item.type}</h2> 
+              <p>{item.breakfast}</p> <hr />
+              <p>{item.morningSnack}</p> <hr />
+              <p>{item.lunch}</p> <hr />
+              <p>{item.afternoonSnack}</p> <hr />
+              <p>{item.dinner}</p> <hr />
+              
+              
+            </div>
+          ))}
+        </div>
+      </div>
 
     </>
   )
 }
 
-export default Diets
+export default Diets;
